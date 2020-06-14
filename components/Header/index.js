a798e842b47f32e820cb71001fe91b39500139e2
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-06-14 19:37:09 -0400

    header complete

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,6 +11,26 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-pull before starting
 
-function Header() {}
+
+function Header() {
+    const headerParent = document.createElement('div');
+    const dateSpan = document.createElement('span');
+    const h1Title = document.createElement('h1');
+    const tempSpan = document.createElement('span');
+
+    headerParent.appendChild(dateSpan);
+    headerParent.appendChild(h1Title);
+    headerParent.appendChild(tempSpan);
+
+    headerParent.classList.add('header');
+    dateSpan.classList.add('date');
+    tempSpan.classList.add('temp');
+
+return headerParent;
+};
+
+const headerContainer = document.querySelector('.header-container');
+
+headerContainer.append(Header());
+console.log(headerContainer);
