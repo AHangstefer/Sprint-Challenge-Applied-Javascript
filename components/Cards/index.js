9ae0e12ae0b52a358759bbe9a961d45f581b1afb
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-06-14 22:00:59 -0400

    working on cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,49 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+
+
+// const articleTopics = ['bootstrap', 'javascript', 'jquery', 'node', 'technology'];
+// articleTopics.forEach((i)=>{
+
+axios
+.get(`https://lambda-times-backend.herokuapp.com/articles/`)
+.then((res)=>{
+    console.log ('Yes! This is the res:', res)
+    res.data.articles.bootstrap.forEach((i)=>{
+    const cardsContainer =document.querySelector('.cards-container');
+    cardsContainer.appendChild(card())
+    })
+.catch((err)=>{
+    console.log('What the hell?!:', err)
+     })
+ });
+
+function card(items){
+    const cardParent = document.createElement('div');
+    const titleH = document.createElement('div');
+    const authorInfo = document.createElement('div');
+    const imageContainer = document.createElement('div');
+    const image = document.createElement('img');
+    const authorName = document.createAttribute('span');
+
+    cardParent.appendChild(titleH);
+    cardParent.appendChild(authorInfo);
+    authorInfo.appendChild(imageContainer);
+    imageContainer.appendChild(image);
+    authorInfo.appendChild(authorName);
+
+    cardParent.classList.add('card');
+    titleH.classList.add('headline');
+    authorInfo.classList.add('author');
+    imageContainer.classList.add('img-container');
+
+    titleH.textContent = (items['headline']);
+    image.src = (items['authorPhoto']);
+    //authorName = (items['authorName']);
+
+
+
+ console.log(cardParent);
+ return cardParent;
+};
